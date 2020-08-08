7e03fdd1aadbdd5bae56c44ca30b1916668436fb
Alex Kruschek <alex.kruschek@gmail.com>
2020-08-08 17:03:15 -0500

    feat: added to Resource and tests

--- app/src/main/java/com/example/incrementalcounter/Resource.java
+++ app/src/main/java/com/example/incrementalcounter/Resource.java
@@ -3,10 +3,14 @@ package com.example.incrementalcounter;
 public class Resource {
 
     private String name;
+    private int total;
+    private double rate;
 
     public Resource(String name)
     {
         this.name = name;
+        total = 0;
+        rate = 1.0;
     }
 
     public String getName()
@@ -14,4 +18,18 @@ public class Resource {
         return name;
     }
 
+    public int getTotal()
+    {
+        return total;
+    }
+
+    public void autoCollection()
+    {
+        total += rate;
+    }
+
+    public void manualCollection(int clickRate)
+    {
+        total += clickRate;
+    }
 }
--- app/src/test/java/com/example/incrementalcounter/ResourceTest.java
+++ app/src/test/java/com/example/incrementalcounter/ResourceTest.java
@@ -6,8 +6,29 @@ import static org.junit.Assert.assertEquals;
 
 public class ResourceTest {
     @Test
-    public void addition_isCorrect() {
+    public void resourceNameIsCorrect()
+    {
         Resource wood = new Resource("Wood");
         assertEquals("Wood", wood.getName());
+    }
+
+    @Test
+    public void resourceRateAddsCorrectly()
+    {
+        Resource wood = new Resource("wood");
+        wood.autoCollection();
+
+        assertEquals(1, wood.getTotal());
+    }
+
+    @Test public void resourceManualCollectionWorks()
+    {
+        Resource wood = new Resource("wood");
+
+        wood.manualCollection(1);
+        assertEquals(1, wood.getTotal());
+
+        wood.manualCollection(7);
+        assertEquals(8, wood.getTotal());
     }
 }
