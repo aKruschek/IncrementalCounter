53ddfd1020a835fd8f5cac3649a4a061d5cd9870
Alex Kruschek <alex.kruschek@gmail.com>
2020-08-09 16:31:49 -0500

    feat: Added textview to upgrades

--- app/src/main/java/com/example/incrementalcounter/Application.java
+++ app/src/main/java/com/example/incrementalcounter/Application.java
@@ -4,22 +4,39 @@ import android.content.res.Configuration;
 
 public class Application extends android.app.Application {
     private Resource[] resources;
+    private Item[] items;
 
     @Override
     public void onCreate() {
         super.onCreate();
         // Required initialization logic here!
 
-        Resource[] resources= {new Resource("Wood"), new Resource("Metal")};
+        Resource wood = new Resource("Wood");
+        Resource metal = new Resource("Metal");
+        Resource[] resources= {wood, metal };
         this.resources = resources;
         Calculator calc = new Calculator(this.resources);
 
         calc.start();
+
+        ItemCost[] pickaxeMaterials = {new ItemCost(wood, 20), new ItemCost(metal, 30)};
+        ItemCost[] axeMaterials = {new ItemCost(wood, 30), new ItemCost(metal, 20)};
+
+        Item pickaxe = new Item("Pickaxe", pickaxeMaterials, metal);
+        Item axe = new Item("Axe", axeMaterials, wood);
+
+        this.items = new Item[]{pickaxe, axe};
+
     }
 
     public Resource[] getGameResources()
     {
         return this.resources;
+    }
+
+    public Item[] getGameItems()
+    {
+        return this.items;
     }
 
     // Called by the system when the device configuration changes while your component is running.
--- app/src/main/java/com/example/incrementalcounter/Upgrades.java
+++ app/src/main/java/com/example/incrementalcounter/Upgrades.java
@@ -3,12 +3,28 @@ package com.example.incrementalcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Upgrades extends AppCompatActivity {
+
+    private Item[] items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upgrades);
+
+        Application appState = ((Application)getApplicationContext());
+        this.items = appState.getGameItems();
+
+        TextView textView = findViewById(R.id.pickaxeName);
+        textView.setText(items[0].getName());
+        textView = findViewById(R.id.axeName);
+        textView.setText(items[1].getName());
+
+        textView = findViewById(R.id.pickaxeOwned);
+        textView.setText(Integer.toString(items[0].getOwned()));
+        textView = findViewById(R.id.axeOwned);
+        textView.setText(Integer.toString(items[1].getOwned()));
     }
 }
