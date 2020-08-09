f7befc5e754d583476ded6e6c0b364d6579ba538
Alex Kruschek <alex.kruschek@gmail.com>
2020-08-09 16:07:39 -0500

    feat: calc test

--- app/src/test/java/com/example/incrementalcounter/CalculatorTest.java
+++ app/src/test/java/com/example/incrementalcounter/CalculatorTest.java
@@ -2,8 +2,23 @@ package com.example.incrementalcounter;
 
 import org.junit.Test;
 
-//public class CalculatorTest {
+import static org.junit.Assert.*;
 
-//    @Test
-//    public void
-//}
+
+public class CalculatorTest {
+
+    @Test
+    public void canIncrementResourcesWithCalc() throws InterruptedException {
+        Resource[] resources= {new Resource("Wood"), new Resource("Metal")};
+        Calculator calc = new Calculator(resources);
+
+       calc.start();
+
+        Thread.sleep(1000);
+
+        assertTrue(resources[0].getTotal() > 0);
+        assertTrue(resources[1].getTotal() > 0);
+    }
+
+
+}
